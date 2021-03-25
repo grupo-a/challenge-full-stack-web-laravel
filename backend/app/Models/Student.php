7c9d57daa2e26bc670030f79b105332d2cd23d27
Diego Vissini <diego@vissini.com.br>
2021-03-24 22:15:35 -0300

    Created Student Model

---
+++ backend/app/Models/Student.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    protected $fillable = [
+        'id','name', 'email', 'ra', 'cpf'
+    ];
+
+}
