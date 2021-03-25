952343c0fa32ffcf3df1e0d48db2716051aed9bf
Diego Vissini <diego@vissini.com.br>
2021-03-24 22:12:32 -0300

    Created student migration

---
+++ backend/database/migrations/2021_03_15_012358_create_students_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100)->require();
+            $table->string('email', 150)->require();
+            $table->string('cpf', 14)->require();
+            $table->string('ra', 20)->unique()->require();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
