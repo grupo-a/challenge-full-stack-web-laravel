50a4e90b7e208a6fda0c5dff024e3e34d59ec0db
Willian Oliveira <wijloc@gmail.com>
2021-04-12 20:22:54 -0300

    feat: add button component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
-    <Header />
-    <router-view></router-view>
+    <v-app>
+      <Header />
+      <router-view></router-view>
+    </v-app>
   </div>
 </template>
 
---
+++ frontend/src/components/Button.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-btn
+    elevation="1"    
+    :to=this.route
+    append
+  >
+    {{ this.caption }}
+  </v-btn>
+</template>
+
+<script>
+export default {
+  props: ["route", "caption"],    
+};
+</script>
