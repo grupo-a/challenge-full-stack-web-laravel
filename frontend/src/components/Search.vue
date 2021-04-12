02f21accf8587ea55df2cc23626b03260e98db30
Willian Oliveira <wijloc@gmail.com>
2021-04-12 20:23:04 -0300

    feat: add search component

---
+++ frontend/src/components/Search.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="content">
+    <v-text-field v-model="searchTerm" :loading="loading"></v-text-field>
+    <v-btn v-on:click="handleSearch"> Buscar </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["onSearch"],
+  data() {
+    return {
+      loading: false,
+      searchTerm: null,
+    };
+  },
+  monted() {
+    this.s = this.searchTerm;
+  },
+  methods: {
+    handleSearch() {
+      this.loading = true;
+      try {
+        this.onSearch(this.searchTerm);
+      } finally {
+        this.loading = false;
+      }
+    },
+  },
+};
+</script>
