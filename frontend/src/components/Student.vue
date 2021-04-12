80d25b61892803b2eae132e80e0e27c4977f8c62
Willian Oliveira <wijloc@gmail.com>
2021-04-12 20:26:18 -0300

    feat: create search field, which searches all columns

--- frontend/src/components/Student.vue
+++ frontend/src/components/Student.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="content">
+    <div class="header">
+      <Search :onSearch="handleSearch" />
+    </div>
     <div class="student-data">
       <v-container>
-        <v-row v-for="student in students" :key="student.id">
+        <v-row v-for="student in filteredStudents" :key="student.id">
           <v-col> {{ student.name }} </v-col>
           <v-col> {{ student.cpf }} </v-col>
           <v-col> {{ student.email }} </v-col>
@@ -15,18 +18,49 @@
 
 <script>
 import api from "../services/api";
+import Search from "@/components/Search";
 
 export default {
   name: "Student",
+  components: {
+    Search,
+  },
   data() {
     return {
       students: [],
+      searchTerm: null,
+      filteredStudents: this.students,
     };
   },
   mounted() {
     api
       .get("/students")
       .then((response) => (this.students = response.data.data));
+  },
+  watch: {
+    students: function (val) {
+      this.filteredStudents = val;
+    },
+    searchTerm: function (val) {
+      if (val == "") {
+        this.filteredStudents = this.students;
+        return;
+      }
+
+      this.filteredStudents = this.students.filter((student) => {
+        return (
+          student.name.includes(val) ||
+          student.cpf.includes(val) ||
+          student.email.includes(val) ||
+          student.academic_register.includes(val)
+        );
+      });
+    },
+  },
+  methods: {
+    handleSearch(searchTerm) {
+      this.searchTerm = searchTerm;
+    },    
   },
 };
 </script>
