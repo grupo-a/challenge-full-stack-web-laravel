b3d155534801269d9b30f956c2c94fdf2918e4be
Willian Oliveira <wijloc@gmail.com>
2021-04-12 22:22:50 -0300

    build: add vue-simple-alert dependency

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
+import VueSimpleAlert from "vue-simple-alert";
+
+Vue.use(VueSimpleAlert);
 
 Vue.config.productionTip = false
 
