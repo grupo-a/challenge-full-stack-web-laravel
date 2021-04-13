583eb29a34bae596fda2c87bd8a467fdaf4cdec9
Willian Oliveira <wijloc@gmail.com>
2021-04-13 00:51:52 -0300

    feat: change colors of style

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -4,4 +4,14 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: '#db1639',
+        secondary: '#00aeb5',
+        accent: '#8c9eff',
+        error: '#b71c1c',
+      },
+    },
+  },
 });
